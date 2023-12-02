bcbe5eb5884f346a95724624c1000b48279ebf50
Gonzalo Ebel <108440757+GonzaAhrexd@users.noreply.github.com>
2023-12-02 21:31:49 +0000

    Improvement

--- member.js
+++ member.js
@@ -1,3 +1,55 @@
 function skillsMember() {
-    console.log('I am a member of the skills.js file')
+    return {
+        name: 'Member',
+        skills: [
+            {
+                name: 'Member',
+                type: 'passive',
+                image: 'member',
+                tooltip: 'The member is a basic unit of any guild. They can be trained to perform various tasks.',
+                manaCost: function (level) {
+                    return 10 + (level * 10);
+                },
+                duration: function (level) {
+                    return 0;
+                },
+                cooldown: function (level) {
+                    return 0;
+                },
+                effect: function (level) {
+                    return 0;
+                },
+                base: {
+                    cost: 10,
+                    duration: 0,
+                    cooldown: 0,
+                    effect: 0
+                },
+                max: {
+                    cost: 100,
+                    duration: 0,
+                    cooldown: 0,
+                    effect: 0
+                },
+                increment: {
+                    cost: 10,
+                    duration: 0,
+                    cooldown: 0,
+                    effect: 0
+                },
+                cost: function (level) {
+                    return 10 + (level * 10);
+                },
+                duration: function (level) {
+                    return 0;
+                },
+                cooldown: function (level) {
+                    return 0;
+                },
+                effect: function (level) {
+                    return 0;
+                }
+            }
+        ]
+    };
 }
